3d8ed191744645d2bdafb3c3b92df213ca8f5128
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2025-04-22 11:25:35 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from '@/components/About';
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Certifications from "@/components/Certifications";
@@ -14,6 +15,7 @@ export default function Home() {
       <Navbar />
       <main className="pt-20">
         <Hero />
+        <About/>
         <Skills />
         <BackToTop />
         <Projects />
